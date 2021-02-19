87269cab210e9f4e27cb8229cbcf48c04aa50c6b
AnastasiaZAYU <anastasiya7.0312@gmail.com>
2021-02-20 00:14:00 +0200

    Test swap the first elements

--- test_list.py
+++ test_list.py
@@ -20,6 +20,9 @@ class TestListMethods(unittest.TestCase):
     def test_kostiantyn_baievskyi_fi_93(self):
         self.assertEqual([1, 2, 3] + [4, 5, 6], [1, 2, 3, 4, 5, 6])
 
+    def test_Anastasia_Zatsarenko_FI94(self):
+        list1 = [2, 1, 3, 4, 5]
+        self.assertEqual(swap_the_first_elements(list1), [1, 2, 3, 4, 5])
 
 if __name__ == '__main__':
     unittest.main()
